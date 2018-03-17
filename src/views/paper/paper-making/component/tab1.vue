96e668d46c527e1ade1a17789127284255501a56
didiaochenmonan <l>
2018-03-17 20:38:51 +0800

    修改了试卷制定

---
+++ src/views/paper/paper-making/component/tab1.vue
@@ -0,0 +1,153 @@
+
+<!-- 试卷制定 -->
+<style lang="less">
+
+    @import '../../../../styles/common.less';
+    .w-e-text-container{
+        height: 200px !important;
+    }
+</style>
+
+<template>
+
+    <Card>
+        <div class="margin-bottom-10 clearfix">
+            <h3 class="fl" style='padding-top:5px;'>试题标题：</h3>
+            <Input v-model="titleName" placeholder="输入试题标题..." style="width: 300px"></Input>
+
+        </div>
+        <div>
+            <Select v-model="model1" style="width:200px" class='margin-right-10'>
+                <Option v-for="item in cityList" :value="item" :key="item">{{ item }}</Option>
+            </Select>
+            <Select v-model="model2" style="width:200px" class='margin-right-10'>
+                <Option v-for="item in cityList2" :value="item" :key="item">{{ item }}</Option>
+            </Select>
+            <Select v-model="model3" style="width:200px">
+                <Option v-for="item in cityList3" :value="item" :key="item">{{ item }}</Option>
+            </Select>
+            <Input v-model="searchConName" icon="search" @on-click="screenOrder" placeholder="搜索..." style="width: 200px" />
+            <Checkbox v-model="single" @on-change='chang'>Checkbox</Checkbox>
+        </div>
+        <div class="clearfix">
+            <Row class="margin-top-10 searchable-table-con1" justify="center" align="middle">
+                <Table border :columns="orderColumns" :data="orderData"></Table>
+            </Row>
+            <div class="clearfix">
+                <Page :total="100" class='fr padding-top-10'></Page>
+            </div>
+        </div>
+    </Card>
+
+</template>
+
+<script>
+import E from 'wangeditor'
+export default {
+    name: 'aaaa',
+    data () {
+        return {
+            single:false,
+            titleName:'',
+            cityList: ['电力类','变电类','通用类'],
+            cityList2: ['选择题','填空题','判断题'],
+            cityList3: ['难','中等','简单'],
+            model1: '电力类',
+            model2: '选择题',
+            model3: '难',
+            searchConName:'',
+            orderColumns: [
+                {
+                    type: 'index',
+                    width: 60,
+                    align: 'center'
+                },
+                {
+                    title: '试卷标题',
+                    key: 'goodsNo',
+                    align: 'center'
+                },
+                {
+                    title: '难度',
+                    key: 'difficulty',
+                    align: 'center'
+                },
+
+                {
+                    title: '操作',
+                    key: 'operation',
+                    align: 'center',
+                    render: (h, params) => {
+                            return h('div', [
+                                h('Checkbox', {
+                                    props: {
+                                        value:params.row.single
+                                    },
+                                    style: {
+                                        marginRight: '10px'
+                                    },
+                                    on: {
+                                        onChange: () => {
+                                            this.btn(params.index)
+                                        }
+                                    }
+                                }),
+
+                            ])
+                    }
+                }
+            ],
+
+            orderData: [
+                {
+                    goodsNo: '国际标准说明',
+                    difficulty:'中等',
+                    single: true,
+
+                },
+                {
+                    goodsNo: '国际标准说明',
+                    difficulty:'中等',
+                    single: false,
+
+                },
+                {
+                    goodsNo: '国际标准说明',
+                    difficulty:'中等',
+                    single: false,
+
+                },
+                {
+                    goodsNo: '国际标准说明',
+                    difficulty:'中等',
+                    single: false,
+
+                },
+
+
+            ],
+
+        };
+    },
+    computed: {
+
+    },
+    methods:{
+        screenOrder(){ //点击搜索
+
+
+        },
+        btn(val){
+            console.log(val)
+
+        },
+        chang(val){
+            console.log(val)
+
+        }
+    },
+    mounted() {
+
+      }
+};
+</script>
--- src/views/paper/paper-making/paper-making.vue
+++ src/views/paper/paper-making/paper-making.vue
@@ -1,5 +1,5 @@
 
-<!-- 试卷上传 -->
+<!-- 试卷制定 -->
 <style lang="less">
 
     @import '../../../styles/common.less';
@@ -11,40 +11,27 @@
 <template>
 
     <Card>
-        <div class="margin-bottom-10 clearfix">
-            <h3 class="fl" style='padding-top:5px;'>试题分类：</h3>
-            <Select v-model="model1" style="width:200px" class='margin-right-10'>
-                <Option v-for="item in cityList" :value="item" :key="item">{{ item }}</Option>
-            </Select>
-            <Select v-model="model2" style="width:200px" class='margin-right-10'>
-                <Option v-for="item in cityList2" :value="item" :key="item">{{ item }}</Option>
-            </Select>
-            <Select v-model="model3" style="width:200px">
-                <Option v-for="item in cityList3" :value="item" :key="item">{{ item }}</Option>
-            </Select>
-        </div>
-        <div class="clearfix">
-            <h3 class="fl">试题内容：</h3>
-            <div class="fl" style='width:650px;'>
-                <div ref="editor" style="text-align:left"></div>
-            </div>
-        </div>
+        <Tabs value="name1">
+            <TabPane label="手动抽题" name="name1"><tab1></tab1></TabPane>
+            <TabPane label="自动抽题" name="name2"><tab2></tab2></TabPane>
+
+        </Tabs>
     </Card>
 
 </template>
 
 <script>
-import E from 'wangeditor'
+import tab1 from './component/tab1.vue'
+import tab2 from './component/tab2.vue'
 export default {
     name: 'mutative-router',
+    components:{
+        tab1,
+        tab2
+    },
     data () {
         return {
-            cityList: ['电力类','变电类','通用类'],
-            cityList2: ['选择题','填空题','判断题'],
-            cityList3: ['难','中等','简单'],
-            model1: '电力类',
-            model2: '选择题',
-            model3: '难',
+
 
         };
     },
@@ -55,31 +42,7 @@ export default {
 
     },
     mounted() {
-        var editor = new E(this.$refs.editor)
-        editor.customConfig.onchange = (html) => {
-          this.editorContent = html
-        }
-        editor.customConfig.menus = [
-            'head',  // 标题
-            'bold',  // 粗体
-            'fontSize',  // 字号
-            'fontName',  // 字体
-            'italic',  // 斜体
-            'underline',  // 下划线
-            'strikeThrough',  // 删除线
-            'foreColor',  // 文字颜色
-            'backColor',  // 背景颜色
-            'link',  // 插入链接
-            'list',  // 列表
-            'justify',  // 对齐方式
-            'emoticon',  // 表情
-            'image',  // 插入图片
-            'table',  // 表格
-            'undo',  // 撤销
-            'redo'  // 重复
-        ]
-        editor.customConfig.uploadImgShowBase64 = true   // 使用 base64 保存图片
-        editor.create()
+
       }
 };
 </script>
